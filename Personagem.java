1b0cc7b5018aa0f5f0a6e6be204a4d340a56fa40
aluno <aluno@fatec.gov.sp.br>
2023-03-09 16:25:52 -0300

    apostila02(oo) bloco de codigo 2.3.1

--- Personagem.java
+++ Personagem.java
@@ -10,16 +10,55 @@ public class Personagem{
     }
 
     void cacar(){
-        System.out.println(nome + " cacando");
+        if(energia >= 2){
+            System.out.println(nome + " cacando");
+            setEnergia(energia - 2);
+        }else{
+            System.out.println(nome + " sem enegia para cacar");
+        }
+        setFome(fome + 1);
+        setSono(sono + 1);
     }
 
     void comer(){
-        System.out.print(nome + " comendo\n");
+        if(fome >= 1){
+            System.out.print(nome + " comendo\n");
+            setEnergia(energia + 1);
+            setFome(fome - 1);
+        }else
+            System.out.print(nome + " sem fome\n");
     }
      
     void dormir(){
-        System.out.printf("%s dormindo%n", nome);
+        if(sono >= 1){
+            System.out.printf("%s dormindo%n", nome);
+            setEnergia(energia + 1);
+            setSono(sono - 1);
+        }else 
+            System.out.printf("%s sem sono%n", nome);
     }
 
+    void setFome(int fome) {
+        if(fome > 10){
+            System.out.println(nome + " esta morrendo de fome");
+            this.fome = 10;
+        }else
+            this.fome = fome;
+    }
+
+    void setSono(int sono) {
+        if(sono > 10){
+            System.out.println(nome + " esta morrendo de sono");
+            this.sono = 10;
+        }else
+            this.sono = sono;
+    }
+
+    void setEnergia(int energia){
+        if(energia > 10)
+            this.energia = 10;
+        else
+            this.energia = energia;
+    }
 
 }
